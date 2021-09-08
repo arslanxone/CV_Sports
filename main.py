d716a2b09375299468f5eaab93ca358a459e8277
arslan.arshad <arshad@illogic.xyz>
2021-09-08 11:53:04 +0200

    changes

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ def pre_processing(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     #applying thresholding
-    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
     return thresh
 
 
-video_path = '../sample_1.mp4'
+video_path = '../sample_1.mp4' #change path of the video where you downloaded in the PC
 cap = cv2.VideoCapture(video_path)
 pre_ret, pre_image = cap.read()
+pre_image_thresh = pre_processing(pre_image)
 
 while True: 
     ret, image = cap.read()
@@ -28,13 +29,15 @@ while True:
         print("Can't receive frame (stream end?). Exiting ...")
         break    
     
-    pre_image_thresh = pre_processing(pre_image)
     image_thresh = pre_processing(image)
 
     img_bwo = cv2.bitwise_or(pre_image_thresh, image_thresh)
+    
+    pre_image_thresh = image_thresh
 
     cv2.imshow('bwo_output', img_bwo)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):               
         break
-    
-    pre_image = image.copy()
+
+cap.release()
+cv2.destroyAllWindows() 
