a7f80e26bc9d80c9ed5949fd9f9dfa354f846d19
arslan.arshad <arshad@illogic.xyz>
2021-09-08 11:24:49 +0200

    added a basic structure

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CV_Sports
+
+Mandatory step to after creating venv. If we didn't do this pip installation of opencv will fail. 'skbuild error'
+
+python -m pip install --upgrade pip
---
+++ main.py
@@ -0,0 +1,40 @@
+import cv2
+
+def pre_processing(image):
+    '''
+    Return:
+        processed_image: Mandatory pre processing steps to get the better results from OCR.
+    Parameters:
+        image: image needed to be processed. 
+    '''
+    #converting it to the grayscale image
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    #applying thresholding
+    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+    return thresh
+
+
+video_path = '../sample_1.mp4'
+cap = cv2.VideoCapture(video_path)
+pre_ret, pre_image = cap.read()
+
+while True: 
+    ret, image = cap.read()
+
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break    
+    
+    pre_image_thresh = pre_processing(pre_image)
+    image_thresh = pre_processing(image)
+
+    img_bwo = cv2.bitwise_or(pre_image_thresh, image_thresh)
+
+    cv2.imshow('bwo_output', img_bwo)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+    pre_image = image.copy()
